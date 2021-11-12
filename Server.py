ee60ed7fb9ca44f99a39423d261edaffdf75e427
Santi <santisz7897@gmail.com>
2021-11-12 16:06:03 +0200

    added Server.py

--- Server.py
+++ Server.py
@@ -14,6 +14,7 @@ while True:
     print("Sent:", data.upper())
     data = client_socket.recv(100)
     print('Received: ', data)
+
     client_socket.send(data.upper())
     print("Sent:", data.upper())
     client_socket.close()
