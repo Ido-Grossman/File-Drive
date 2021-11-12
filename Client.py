a66c8bbc98e088f1dd161a1d93155c0e6fa41efe
idodd <idoddii@gmail.com>
2021-11-12 16:01:34 +0200

    Client

---
+++ Client.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+
+if len(sys.argv) != 2:
+    exit(-1)
+ip = sys.argv[1]
+port = int(sys.argv[2])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ip, port))
+s.send(b'Ido Grossman: 208985424')
+data = s.recv(100)
+print("Server sent: ", data)
+s.send(b'Santiago Sterenberg: 3246034220')
+data = s.recv(100)
+print("Server sent: ", data)
+s.close()
