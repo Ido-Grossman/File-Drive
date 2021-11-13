df67eaf3fd4b66ed442381af536d26810401aa6f
idodd <idoddii@gmail.com>
2021-11-13 13:00:34 +0200

    Client

---
+++ Handler.py
@@ -0,0 +1,18 @@
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+class Handler(FileSystemEventHandler):
+    def on_deleted(self, event):
+        return None
+
+    def on_closed(self, event):
+        return None
+
+    def on_moved(self, event):
+        return None
+
+    def on_created(self, event):
+        return None
+
+    def on_modified(self, event):
+        return None
