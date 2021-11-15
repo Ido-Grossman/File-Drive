74b5c9a697223900eaaf62ca072e0c07b28322d9
idodd <idoddii@gmail.com>
2021-11-15 18:55:43 +0200

    succeed first interaction

--- Client.py
+++ Client.py
@@ -4,6 +4,9 @@ import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 100000
 files = list()
 if len(sys.argv) < 5:
     exit(-1)
@@ -20,16 +23,45 @@ files = os.walk(path, True)
 def noIdentifier():
     global identifier, files
     s.send("Hello, i am new here".encode('utf-8'))
-    identifier = s.recv(128).decode('utf-8')
+    identifier = s.recv(130).decode('utf-8')
+    print(identifier)
     files = os.walk(path, True)
     for (dirpath, dirnames, filename) in files:
-        s.send(("the path is:" + dirpath).encode('utf-8'))
+        s.send("the path is:".encode('utf-8'))
+        s.recv(100)
+        localPath = str(dirpath).removeprefix(path)
+        seperator = ""
+        if path.find('\\'):
+            seperator = '\\'
+        else:
+            seperator = '/'
+        pathToFolder = localPath.split(seperator)
+        if len(pathToFolder) > 1:
+            for i in range(1, len(pathToFolder)):
+                s.send(pathToFolder[i].encode('utf-8'))
+                s.recv(100)
         s.send("the directories are:".encode('utf-8'))
+        s.recv(100)
         for directory in dirnames:
             s.send(directory.encode('utf-8'))
+            s.recv(100)
         s.send("the files are:".encode('utf-8'))
+        s.recv(100)
         for file in filename:
+            filepath = os.path.join(dirpath, file)
+            filesize = str(os.path.getsize(filepath))
             s.send(file.encode('utf-8'))
+            s.recv(100)
+            s.send(filesize.encode('utf-8'))
+            s.recv(100)
+            f = open(filepath, "rb")
+            while True:
+                bytes_read = f.read(4096)
+                if not bytes_read:
+                    break
+                s.send(bytes_read)
+            s.recv(100)
+            f.close()
 
 
 # while True:
@@ -37,4 +69,5 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
 if identifier is None:
     noIdentifier()
+s.send("I have finished".encode('utf-8'))
 s.close()
