fc326e8633e9bcf1721a944fa7e134250637ccdf
idodd <idoddii@gmail.com>
2021-11-16 21:36:52 +0200

    succeed first interaction

--- Client.py
+++ Client.py
@@ -16,15 +16,36 @@ port = int(sys.argv[2])
 path = sys.argv[3]
 timeOut = int(sys.argv[4])
 if len(sys.argv) == 6:
+    if (len(sys.argv[5]) < 128):
+        exit(-1)
     identifier = sys.argv[5]
 files = os.walk(path, True)
+finishMessage = "I have finished"
 
 
-def noIdentifier():
+def recvFile():
+    s.send("empty directory".encode('utf-8'))
+    message = s.recv(100).decode('utf-8')
+    while message != finishMessage:
+        currPath = path
+        while message != "the directories are:":
+            message = s.recv(100).decode('utf-8')
+            s.send(b'hi')
+            os.path.join(currPath, message)
+        while message != "the files are:":
+            message = s.recv(100).decode('utf-8')
+            s.send(b'hi')
+            dirPath = os.path.join(currPath, message)
+            os.mkdir(dirPath)
+        while message != "the path is:" or message != finishMessage:
+            message = s.recv(100).decode('utf-8')
+            s.send(b'hi')
+            filePath = os.path.join(currPath, message)
+            file = open(filePath, "wb")
+
+
+def sendAll():
     global identifier, files
-    s.send("Hello, i am new here".encode('utf-8'))
-    identifier = s.recv(130).decode('utf-8')
-    print(identifier)
     files = os.walk(path, True)
     for (dirpath, dirnames, filename) in files:
         s.send("the path is:".encode('utf-8'))
@@ -64,10 +85,26 @@ def noIdentifier():
             f.close()
 
 
+def sync():
+    return None
+
+
 # while True:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
 if identifier is None:
-    noIdentifier()
-s.send("I have finished".encode('utf-8'))
+    s.send("Hello, i am new here".encode('utf-8'))
+    identifier = s.recv(130).decode('utf-8')
+    print(identifier)
+    sendAll()
+else:
+    s.send(identifier.encode('utf-8'))
+    message = s.recv(100).decode('utf-8')
+    if message == "found you!":
+        sync()
+    elif message == "found you!" and len(os.listdir(path)) == 0:
+        recvFile()
+    else:
+        sendAll()
+s.send(finishMessage.encode('utf-8'))
 s.close()
