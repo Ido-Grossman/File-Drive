d9dab782295e5f787c72320049e744cea5cc5060
idodd <idoddii@gmail.com>
2021-11-16 22:37:49 +0200

    Client

--- Client.py
+++ Client.py
@@ -3,12 +3,9 @@ import sys
 import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
 import utils
 
-SEPARATOR = "<SEPARATOR>"
-BUFFER_SIZE = 100000
-files = list()
+# gets the arguments from the user and check if the user sent all the needed arguments as he should
 if len(sys.argv) < 5:
     exit(-1)
 identifier = None
@@ -17,16 +14,20 @@ port = int(sys.argv[2])
 path = sys.argv[3]
 timeOut = int(sys.argv[4])
 if len(sys.argv) == 6:
-    if (len(sys.argv[5]) < 128):
+    if len(sys.argv[5]) != 128:
         exit(-1)
     identifier = sys.argv[5]
-finishMessage = "I have finished"
+trial = os.sep
 
 
+# sends all the files that are in the folder
 def sendAll(socket):
+    # gets all the files in the folder and for folder inside it sends all the files and folders it contains
     files = os.walk(path, True)
     for (dirpath, dirnames, filenames) in files:
         utils.sendFiles(socket, path, dirpath, dirnames, filenames)
+    # when it finished sending all the files it notifies the server
+    socket.send("I have finished".encode('utf-8'))
         
 
 
@@ -35,21 +36,28 @@ def sync():
 
 
 # while True:
+# gets the socket from the os and connects to the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
+# if this is a new client it notifies the server that he is new and gets is notifier and then sends everything
+# inside the folder and subfolder to the server
 if identifier is None:
     s.send("Hello, i am new here".encode('utf-8'))
     identifier = s.recv(130).decode('utf-8')
     print(identifier)
     sendAll(s)
 else:
+    # If the client already have an identifier he sends it to the server.
     s.send(identifier.encode('utf-8'))
     message = s.recv(100).decode('utf-8')
+    # if the server found the identifier then it syncs all the new changes with the client.
     if message == "found you!":
         sync()
+    # If the server found the identifier and the client folder is empty, the server sends the client everything
     elif message == "found you!" and len(os.listdir(path)) == 0:
+        s.send("empty directory".encode('utf-8'))
         utils.recvFile(s, path)
+    # If the server didn't find the identifier then the client sends everything to the server
     else:
         sendAll(s)
-s.send(finishMessage.encode('utf-8'))
 s.close()
--- utils.py
+++ utils.py
@@ -1,33 +1,39 @@
 import os
 
 
+# Used to send files to the other side, it gets the socket, path to the folder it syncs = path_to_main,
+# path to the current folder he is in = path_to_folder, directories and files inside the folder
 def sendFiles(socket, path_to_main, path_to_folder, directories, files):
+    # it sets the local path of the folder we are in now and notifies the server that he sends the path
     local_path = str(path_to_folder).removeprefix(path_to_main)
     socket.send("the path is:".encode('utf-8'))
     socket.recv(100)
-    if path_to_main.find('\\'):
-        separator = '\\'
-    else:
-        separator = '/'
+    # it finds how the folder are seperated in the os and splits them and sends each folder by order to get into the
+    # desired path
+    separator = os.sep
     path_to_folder = local_path.split(separator)
     if len(path_to_folder) > 1:
         for i in range(1, len(path_to_folder)):
             socket.send(path_to_folder[i].encode('utf-8'))
             socket.recv(100)
+    # after it finished sending the path it sends all the directories in the path
     socket.send("the directories are:".encode('utf-8'))
     socket.recv(100)
     for directory in directories:
         socket.send(directory.encode('utf-8'))
         socket.recv(100)
+    # after it finished sending all the directories in the path if sends all the files in the path
     socket.send("the files are:".encode('utf-8'))
     socket.recv(100)
     for file in files:
+        # it gets the path to the file and gets is size and name and sends them to the server
         filepath = os.path.join(str(path_to_folder), file)
         filesize = str(os.path.getsize(filepath))
         socket.send(file.encode('utf-8'))
         socket.recv(100)
         socket.send(filesize.encode('utf-8'))
         socket.recv(100)
+        # we open the file in read bytes mode and send all the bytes in the file to the server.
         f = open(filepath, "rb")
         while True:
             bytes_read = f.read(4096)
@@ -39,20 +45,26 @@ def sendFiles(socket, path_to_main, path_to_folder, directories, files):
 
 
 def recvFile(socket, path_to_main):
-    socket.send("empty directory".encode('utf-8'))
     message = socket.recv(100).decode('utf-8')
+    # while the other side didn't finish sending all the files it tries to get to the next path
     while message != "I have finished":
+        # saves the path we currently in
         curr_path = str(path_to_main)
         while message != "the directories are:":
+            # until we get the message "the directories are:" it means the other side still sends us the path
+            # so we join each folder to the current path
             message = socket.recv(100).decode('utf-8')
             socket.send(b'hi')
             os.path.join(curr_path, message)
         while message != "the files are:":
+            # until we get the message "the files are:" it means we still get the directories so we create them.
             message = socket.recv(100).decode('utf-8')
             socket.send(b'hi')
             dir_path = os.path.join(curr_path, message)
             os.mkdir(dir_path)
         while message != "the path is:" or message != "I have finished":
+            # until the other side has finished or sends us another path we create the files in the path
+            # we get the name and size of file each time and open the file
             message = socket.recv(100).decode('utf-8')
             socket.send(b'hi')
             file_path = os.path.join(curr_path, message)
@@ -61,6 +73,7 @@ def recvFile(socket, path_to_main):
             file_size = int(message)
             file = open(file_path, "wb")
             counter = 0
+            # until we finished reading all the file, we will receive the next bytes and write them to the file.
             while counter < file_size:
                 # read 1024 bytes from the socket (receive)
                 bytes_read = socket.recv(100000)
