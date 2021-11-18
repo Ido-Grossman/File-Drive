af0777e24a98b1e06e6f058262136cd8d47cf730
Shoham Elkayam <318941788@idf.il>
2021-11-18 15:40:45 +0200

    finished recvFile

--- utils.py
+++ utils.py
@@ -2,75 +2,34 @@ import os
 import string
 import random
 
-# Used to send files to the other side, it gets the socket, path to the folder it syncs = path_to_main,
-# path to the current folder he is in = path_to_folder, directories and files inside the folder
-def sendFiles(socket, path_to_main, path_to_folder, directories, files):
-    # it sets the local path of the folder we are in now and notifies the server that he sends the path
-    local_path = str(path_to_folder).removeprefix(path_to_main)
-    socket.send("the path is:".encode('utf-8'))
-    socket.recv(100)
-    separator = os.sep
-    folders = local_path.split(separator)
-    if len(folders) > 1:
-        for i in range(1, len(folders)):
-            socket.send(folders[i].encode('utf-8'))
-            socket.recv(100)
-    socket.send("the directories are:".encode('utf-8'))
-    socket.recv(100)
-    # after it finished sending the path it sends all the directories in the path
-    for directory in directories:
-        socket.send(directory.encode('utf-8'))
-        socket.recv(100)
-    # after it finished sending all the directories in the path if sends all the files in the path
-    socket.send("the files are:".encode('utf-8'))
-    socket.recv(100)
-    for file in files:
-        # it gets the path to the file and gets is size and name and sends them to the server
-        filepath = os.path.join(path_to_folder, file)
-        filesize = str(os.path.getsize(filepath))
-        socket.send(file.encode('utf-8'))
-        socket.recv(100)
-        socket.send(filesize.encode('utf-8'))
-        socket.recv(100)
-        # we open the file in read bytes mode and send all the bytes in the file to the server.
-        f = open(filepath, "rb")
-        while True:
-            bytes_read = f.read(4096)
-            if not bytes_read:
-                break
-            socket.send(bytes_read)
-        socket.recv(100)
-        f.close()
-
 
 def recvFile(socket, path_to_main):
     message = socket.recv(100).decode('utf-8')
+    curr_path = path_to_main
     # while the other side didn't finish sending all the files it tries to get to the next path
     while message != "I have finished":
-        # saves the path we currently in
-        curr_path = str(path_to_main)
         socket.send(b'hi')
-        message = socket.recv(100).decode('utf-8')
-        socket.send(b'hi')
+        curr_path = path_to_main
+        message = socket.recv(100).decode('utf-8') # waiting for path or the dir...
         while message != "the directories are:":
             curr_path = os.path.join(curr_path, message)
-            message = socket.recv(100).decode('utf-8')
             socket.send(b'hi')
-        message = socket.recv(100).decode('utf-8')
+            message = socket.recv(100).decode('utf-8')
+        socket.send(b'hi')
+        message = socket.recv(100).decode('utf-8')  # waiting for name of dir or the files are...
         while message != "the files are:":
             # until we get the message "the files are:" it means we still get the directories so we create them.
             socket.send(b'hi')
             dir_path = os.path.join(curr_path, message)
             os.mkdir(dir_path)
             message = socket.recv(100).decode('utf-8')
-            print(message)
+        socket.send(b'hi')
+        message = socket.recv(100).decode('utf-8') # waiting for name of file or the path or finished..
         while message != "the path is:":
             # until the other side has finished or sends us another path we create the files in the path
             # we get the name and size of file each time and open the file
             if message == "I have finished":
                 break
-            socket.send(b'hi')
-            message = socket.recv(100).decode('utf-8')
             socket.send(b'hi')
             file_path = os.path.join(curr_path, message)
             message = socket.recv(100).decode()
