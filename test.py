57ae7b7b951d5fe4893f160f14c57f47f8fc82cd
idodd <idoddii@gmail.com>
2021-11-22 01:40:59 +0200

    Client

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import utils
 import os
+import socket
 
 handler = utils.Handler()
 w = utils.Watcher('ToSync', handler)
-w.run(30)
+w.run(15)
 print(handler.changes)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect(('25.84.19.151', 12345))
+# s.send(str(True).encode('utf-8'))
+# s.close()
